9d074ebae8b3a4781edd2f813406db2146221798
Pogito <pogito@prince.localdomain>
2022-01-19 13:21:23 -0500

    Sign Up Screen UI Finished

--- lib/screens/authentication_screen.dart
+++ lib/screens/authentication_screen.dart
@@ -10,48 +10,26 @@ class Authentication extends StatefulWidget {
 class _AuthenticationState extends State<Authentication> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: PreferredSize(
-          preferredSize: Size.fromHeight(400),
-          child: AppBar(
-          backgroundColor: Colors.transparent,
-          elevation: 0,
-          flexibleSpace: Container(
-          margin: EdgeInsets.only(top: 5),
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/logo.png"),
-              fit: BoxFit.cover,
-            )
-          )
-        ),
-          ),
-        ),
-        body: AuthForm(),
-        ),
-    );
-    // return Scaffold(
-    //     appBar: PreferredSize(
-    //     preferredSize: Size.fromHeight(10),
-    //     child: AppBar(
-    //       elevation: 0,
-    //       centerTitle: true,
-    //       flexibleSpace: Container(
-    //       margin: EdgeInsets.only(top: 5),
-    //       decoration: BoxDecoration(
-    //         image: DecorationImage(
-    //           image: AssetImage("assets/images/logo.png"),
-    //           fit: BoxFit.cover,
-    //         )
-    //       )
-    //     ),
-    //     backgroundColor: Colors.transparent
-    //     ),
-    //   ),
+    return Scaffold(
+      //   appBar: PreferredSize(
+      //   preferredSize: Size.fromHeight(10),
+      //   child: AppBar(
+      //     elevation: 0,
+      //     centerTitle: true,
+      //     flexibleSpace: Container(
+      //     margin: EdgeInsets.only(top: 5),
+      //     decoration: BoxDecoration(
+      //       image: DecorationImage(
+      //         image: AssetImage("assets/images/logo.png"),
+      //         fit: BoxFit.cover,
+      //       )
+      //     )
+      //   ),
+      //   backgroundColor: Colors.transparent
+      //   ),
+      // ),
       
-    //   body: AuthForm(),
-    // );
+      body: AuthForm(),
+    );
   }
 }
--- lib/widgets/auth_form.dart
+++ lib/widgets/auth_form.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class AuthForm extends StatefulWidget {
 
@@ -9,60 +10,179 @@ class AuthForm extends StatefulWidget {
 class _AuthFormState extends State<AuthForm> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        margin: EdgeInsets.all(20.0),
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Form(
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextFormField(
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                        labelText: 'Username',
-                        contentPadding: const EdgeInsets.all(15.0),
-                        border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Container(
+                height: 370,
+                child: Stack(
+                  children: [
+                    Positioned(
+                      top: 0,
+                      left: -30,
+                      right: -30,
+                      bottom: 0,
+                      child: Image.asset("assets/images/logo.png"),
+                    )
+                  ],
+                ),
+               ),
+               SizedBox(height: 20,),
+               Form(
+                 child: Container(
+                   width: 300,
+                   child: Column(
+                     mainAxisSize: MainAxisSize.min,
+                     children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10.0),
+                        child: TextFormField(
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                            labelText: 'Username',
+                            contentPadding: const EdgeInsets.all(15.0),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextFormField(
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                        labelText: 'Email',
-                        contentPadding: const EdgeInsets.all(15.0),
-                        border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                        ),
-                      ),),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: TextFormField(
-                      keyboardType: TextInputType.text,
-                      decoration: InputDecoration(
-                        labelText: 'Password',
-                        contentPadding: const EdgeInsets.all(15),
-                        border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10.0),
+                        child: TextFormField(
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                            labelText: 'Email',
+                            contentPadding: const EdgeInsets.all(15.0),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
                         ),
                       ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10.0),
+                        child: TextFormField(
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                            labelText: 'Password',
+                            contentPadding: const EdgeInsets.all(15.0),
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.grey.shade200, width: 2),
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10.0),
+                        child: ButtonTheme(
+                          minWidth: 200,
+                          height: 45,
+                          child: MaterialButton(
+                              elevation: 0,
+                              onPressed: (){},
+                              color: Color.fromRGBO(255, 255, 0, 100),
+                              child: Text("Sign up", style: TextStyle(color: Colors.black, fontSize: 18),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                            side: BorderSide(color: Color.fromRGBO(255, 255, 0, 0.5),)
+                          ),
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 180,),
+                      Text("Already have an account?"),
+                      SizedBox(height: 10,),
+                      Padding(
+                        padding: EdgeInsets.only(bottom: 10.0),
+                        child: ButtonTheme(
+                          minWidth: 250,
+                          height: 45,
+                          child: MaterialButton(
+                              elevation: 0,
+                              onPressed: (){},
+                              color: Color.fromRGBO(255, 255, 0, 0.5),
+                              child: Text("Login", style: TextStyle(color: Colors.black, fontSize: 18),
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                            side: BorderSide(color: Color.fromRGBO(255, 255, 0, 100),)
+                          ),
+                          ),
+                        ),
+                      ),
+                      
+                     ],
                     ),
-                  ),
-                ],
-              ),
-            ),
+                 ),
+                )
+            ],
           ),
         ),
       ),
     );
+    // return Center(
+    //   child: Card(
+    //     margin: EdgeInsets.all(20.0),
+    //     child: SingleChildScrollView(
+    //       child: Padding(
+    //         padding: EdgeInsets.all(10.0),
+    //         child: Form(
+    //           child: Column(
+    //             mainAxisSize: MainAxisSize.min,
+    //             children: <Widget>[
+    //               Padding(
+    //                 padding: const EdgeInsets.all(10.0),
+    //                 child: TextFormField(
+    //                   keyboardType: TextInputType.text,
+    //                   decoration: InputDecoration(
+    //                     labelText: 'Username',
+    //                     contentPadding: const EdgeInsets.all(15.0),
+    //                     border: OutlineInputBorder(
+    //                     borderRadius: BorderRadius.circular(10.0),
+    //                     ),
+    //                   ),
+    //                 ),
+    //               ),
+    //               Padding(
+    //                 padding: const EdgeInsets.all(10.0),
+    //                 child: TextFormField(
+    //                   keyboardType: TextInputType.text,
+    //                   decoration: InputDecoration(
+    //                     labelText: 'Email',
+    //                     contentPadding: const EdgeInsets.all(15.0),
+    //                     border: OutlineInputBorder(
+    //                     borderRadius: BorderRadius.circular(10.0),
+    //                     ),
+    //                   ),),
+    //               ),
+    //               Padding(
+    //                 padding: const EdgeInsets.all(10.0),
+    //                 child: TextFormField(
+    //                   keyboardType: TextInputType.text,
+    //                   decoration: InputDecoration(
+    //                     labelText: 'Password',
+    //                     contentPadding: const EdgeInsets.all(15),
+    //                     border: OutlineInputBorder(
+    //                     borderRadius: BorderRadius.circular(10),
+    //                     ),
+    //                   ),
+    //                 ),
+    //               ),
+    //             ],
+    //           ),
+    //         ),
+    //       ),
+    //     ),
+    //   ),
+    // );
   }
 }
